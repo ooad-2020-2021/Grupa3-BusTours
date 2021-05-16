5a52f35d30f77d0ceacd3a525e059ca0fc7c6c76
abegic2 <abegic2@etf.unsa.ba>
2021-05-16 14:08:00 +0200

    Bambus
    Dodan folder implementacije

---
+++ Projekat/ProjectBamBus/ProjectBamBus/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectBamBus.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
