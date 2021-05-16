1d44c7106c479c2178a65802cb25fc1ccf195b07
Zukic98 <73072238+Zukic98@users.noreply.github.com>
2021-05-16 14:27:51 +0200

    Novi komit

---
+++ Projekat/ProjectBamBus/ProjectBamBus/Models/Probna2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectBamBus.Models
+{
+    public class Probna2
+    {
+
+    }
+}
