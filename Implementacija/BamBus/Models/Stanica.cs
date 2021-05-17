0689c56fa534184dddd1857c296a9c25a60f8742
abegic2 <abegic2@etf.unsa.ba>
2021-05-17 15:55:45 +0200

    Dodana Stanica

---
+++ Implementacija/BamBus/Models/Stanica.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BamBus.Models
+{
+	public class Stanica()
+	{
+		public string Grad { get; set; }
+		public long XOsa { get; set; }
+		public long YOsa { get; set; }
+		public string Opis { get; set; }
+
+		public Stanica(string grad, long xOsa, long yOsa, string opis = "")
+		{
+		this.Grad = grad;
+		this.XOsa = xOsa;
+		this.YOsa = yOsa;
+		this.OpisOpis = opis;
+		}
+
+
+		
+	}
+}
