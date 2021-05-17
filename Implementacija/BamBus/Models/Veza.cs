6ebb56c3c095c7f16b2844719d48294911282217
abegic2 <abegic2@etf.unsa.ba>
2021-05-17 16:22:55 +0200

    Dodana Veza

---
+++ Implementacija/BamBus/Models/Veza.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BamBus.Models
+{
+	public class Veza
+	{
+		public Stanica Stanica1 { get; set; }
+		public Stanica Stanica2 { get; set; }
+		public double Cijena { get; set; }
+		public double MedjuVrijeme { get; set; } 
+
+		public Veza()
+        {
+        }
+
+		public Veza(Stanica stanica1, Stanica stanica2, double cijena, double medjuVrijeme)
+		{
+			this.Stanica1 = stanica1;
+			this.Stanica2 = stanica2;
+			this.Cijena = cijena;
+			this.MedjuVrijeme = medjuVrijeme;
+		}
+	}
+
+}
