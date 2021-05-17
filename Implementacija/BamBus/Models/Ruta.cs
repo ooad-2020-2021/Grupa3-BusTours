822e0e48be7d13da5174b338f6648e47da6a348f
abegic2 <abegic2@etf.unsa.ba>
2021-05-17 16:40:31 +0200

    Dodana ruta

---
+++ Implementacija/BamBus/Models/Ruta.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BamBus.Models
+{
+	public class Ruta
+	{
+		public string MapaURL { get; set; }
+		public double Duzina { get; set; }
+		public List<string> AlternativnaMapa { get; set; }
+		public double Troskovi { get; set; }
+		public int Id { get; set; }
+		public List<Veza> VezeStanica { get; set; }
+
+		public Ruta()
+		{
+		}
+
+		public Ruta(string mapaURL, double duzina, List<Veza> vezeStanica, double troskovi, int id) 
+		{
+			this.MapaURL = mapaURL;
+			this.Duzina = duzina;
+			this.Troskovi = troskovi;
+			this.VezeStanica = vezeStanica;
+			this.Id = id;
+		}
+
+		public bool dodajVezu(Veza veza)
+        {
+			if(VezeStanica.Contains(veza))
+            {
+				return false;
+            }
+			VezeStanica.Add(veza);
+			return true;
+        }
+
+		public dodajAlternativnuMapu(string mapa)
+        {
+			if(AlternativnaMapa.Contains(mapa))
+            {
+				return false;
+            }
+			this.AlternativnaMapa.Add(mapa);
+			return true;
+        }
+
+		public bool obrisiVezu(Veza veza)
+        {
+			if(VezeStanica.Contains(veza))
+            {
+				VezeStanica.Remove(veza);
+				return true;
+            }
+			return false;
+        }
+
+		public bool obrisiAlternativnuMapu(string mapa)
+        {
+			if(AlternativnaMapa.Contains(mapa))
+            {
+				AlternativnaMapa.Remove(mapa);
+				return true;
+            }
+			return false;
+        }
+
+
+
+
+	}
+}
