424d5d793dfa07b529a114c92f2dee5785479216
Zukic98 <73072238+Zukic98@users.noreply.github.com>
2021-05-23 17:31:32 +0200

    dodane ostale potrebe

---
+++ Implementacija/BamBus/Models/Korisnik.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BamBus.Models
+{
+    public class Korisnik
+    {
+        public string Ime{get;set; }
+        public string Prezime { get; set; }
+        public DateTime Godiste { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public Korisnik(string Ime,string Prezime,DateTime Godiste,string Email,string Password)
+        {
+            this.Ime = Ime;
+            this.Prezime = Prezime;
+            this.Godiste = Godiste;
+            this.Email = Email;
+            this.Password = Password;
+        }
+    }
+}
---
+++ Implementacija/BamBus/Models/Vozac.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BamBus.Models
+{
+    public class Vozac:Korisnik
+    {
+        public string TekstOUposleniku { get; set; }
+
+        public Vozac(string Ime, string Prezime, DateTime Godiste, string Email, string Password,string Tekst):base(Ime,Prezime, Godiste, Email, Password)
+        {
+            TekstOUposleniku = Tekst;
+        }
+    }
+}
